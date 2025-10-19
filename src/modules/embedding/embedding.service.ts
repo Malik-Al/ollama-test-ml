d57445b9523d15f8f04bad49a9ac90e2da868f1d
unknown <malikimenov@gmail.com>
2025-10-19 20:41:29 +0600

    [Upd]

--- src/modules/embedding/embedding.service.ts
+++ src/modules/embedding/embedding.service.ts
@@ -6,18 +6,31 @@ export class EmbeddingService {
   private model = 'nomic-embed-text'; 
 
   async embedTexts(text: string): Promise<number[]> {
+    console.log('[START] EmbeddingService method embedTexts test: ', text);
+    try {
       const res = await ollama.embed({
         model: this.model,
         input: text,
       });
-    return res.embeddings[0];
+      return res.embeddings[0];
+    } catch (error) {
+      console.error(`[EEROR] EmbeddingService method embedTexts error: `, error);
+      throw error
+    }
   }
 
-  async embedQuestion(question: string) {
-    const res = await ollama.embed({
-      model: this.model,
-      input: question,
-    });
-    return res.embeddings[0]
-  }
+  async embedQuestion(question: string): Promise<number[]> {
+    console.log('[START] EmbeddingService method embedQuestion question: ', question);
+    try {
+      const res = await ollama.embed({
+        model: this.model,
+        input: question,
+      });
+      return res.embeddings[0]
+      
+    } catch (error) {
+      console.error(`[EEROR] EmbeddingService method embedQuestion error: `, error);
+      throw error
+    }
+   }
 }
