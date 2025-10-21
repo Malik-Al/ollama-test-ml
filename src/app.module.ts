3dd0706cef9a23ae39b810cda17187be9b4a7fe8
unknown <malikimenov@gmail.com>
2025-10-22 00:49:23 +0600

    [Upd] install pack and use module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ElasticModule } from './modules/elastic/elastic.module';
 import { ChatModule } from './modules/chat/chat.module';
 import { UploadModule } from './modules/upload/upload.module';
+import { TelegramModule } from './modules/telegram/telegram.module';
 
 @Module({
   imports: [
     ElasticModule,
     UploadModule,
     ChatModule,
+    TelegramModule 
   ],
 })
 export class AppModule {}
